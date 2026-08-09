bf82729d45b80c6c73550b69ff8d55fd24e79079
i.tsachev <tsachev.official@gmail.com>
2026-08-09 17:26:49 +0300

    feat: inter font

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App.jsx";
 import "@fontsource/italiana/400.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/500.css";
 import "@/styles/main.scss";
 
 createRoot(document.getElementById("root")).render(
